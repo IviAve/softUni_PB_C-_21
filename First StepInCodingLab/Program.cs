0ece8f62410f44943f46c6877b5a5bc385cefadd
IvaDeneva <156186907+IviAve@users.noreply.github.com>
2024-01-23 18:02:05 +0200

    firstStep

---
+++ First StepInCodingLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_StepInCodingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
